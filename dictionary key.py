a94dd68457285e74726023db8734b395948f0127
Saimounik428 <saimounik68@gmail.com>
2024-06-22 15:41:54 +0530

    Add files via upload

---
+++ dictionary key.py
@@ -0,0 +1,11 @@
+user_dict={}
+
+num_entries=int(input("Enter the number of entries you want to add"))
+
+for i in range(num_entries):
+     key=input("Enter key:")
+     value=input("Enter value:")
+     user_dict[key]=value
+
+print("dictionary after adding user input:",user_dict)
+
